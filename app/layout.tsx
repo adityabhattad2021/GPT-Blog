7531556e3a86e790f8d324d75107397f48c539d2
Aditya Bhattad <adityabhattad18@gmail.com>
2023-04-22 16:51:24 +0530

    Initial Setup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import { Open_Sans } from 'next/font/google'
+
+const openSans = Open_Sans({
+  subsets:["latin"],
+})
+
+export const metadata = {
+  title: 'AI Blog App',
+  description: 'Nextjs AI blog app.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={openSans.className}>{children}</body>
+    </html>
+  )
+}
